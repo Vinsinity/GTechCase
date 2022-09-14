8886835252262c341ba64c37b3a128f734d3736b
Erhan Hangül <vinsinity.erhan@gmail.com>
2022-09-14 18:49:46 +0300

    Main.storyboard deleted
    - Main.storyboard deleted.
    - Home Controller Added.
    - Folder structure changed for storyboardless.

---
+++ GTechCase/Common/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  GTechCase
+//
+//  Created by Erhan on 14.09.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = HomeViewController()
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+
+}
+
